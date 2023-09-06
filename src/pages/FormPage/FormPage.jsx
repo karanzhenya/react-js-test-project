fa5b2206e1648e28a300697c60ad0325b62339fa
Karan Evgeniy <63317606+karanzhenya@users.noreply.github.com>
2023-09-06 09:02:12 +0300

    Update FormPage.jsx
    refactor submit button

--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -199,7 +199,7 @@ function FormPage(props) {
                         );
                     })}
                     <div className='submitButton'>
-                        <input type="submit"/>
+                        <input type="submit" value='Отправить'/>
                     </div>
                 </form>
             </section>
